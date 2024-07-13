b47d0ada50de46a104e82b7153c70f2529591236
shres707 <mailshreekanth@gmail.com>
2024-07-13 17:41:09 +0530

    Integrating ChatBot

--- multipage_main.py
+++ multipage_main.py
@@ -9,8 +9,8 @@ PAGES = {
     "Input": youtube_comment_scrapper_app,
     "Data Preprocess": data_preprocess_eda_app,
     "EDA"  : eda_app,
-    "Sentiment Analysis": sentiment_app,
-    "ChatBot":chatbot_app
+    "Sentiment Analysis": sentiment_app
+
     
 }
 st.sidebar.title('CRISP DM Navigation')
