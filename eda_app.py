166681a2881f46b929c889e43670dcac00a96e0d
shres707 <mailshreekanth@gmail.com>
2024-07-13 18:10:52 +0530

    Integrating ChatBot

--- eda_app.py
+++ eda_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from ydata_profiling import ProfileReport
-#from streamlit_pandas_profiling import st_profile_report
-from streamlit_ydata_profiling import st_profile_report
+from streamlit_pandas_profiling import st_profile_report
+#from streamlit_ydata_profiling import st_profile_report
 
 def app():
   st.title("EDA")
