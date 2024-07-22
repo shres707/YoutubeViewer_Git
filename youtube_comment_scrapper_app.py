2939c4e168fd7a9003c78607acd137ff8f3e0dac
shres707 <mailshreekanth@gmail.com>
2024-07-22 21:23:24 +0530

    Update youtube_comment_scrapper_app.py
    Extracted comment id

--- youtube_comment_scrapper_app.py
+++ youtube_comment_scrapper_app.py
@@ -52,12 +52,13 @@ def scrape_comments_with_replies(url):
 
         name = i["snippet"]['topLevelComment']["snippet"]["authorDisplayName"]
         comment = i["snippet"]['topLevelComment']["snippet"]["textDisplay"]
+        comment_id = i["id"]
         published_at = i["snippet"]['topLevelComment']["snippet"]['publishedAt']
         likes = i["snippet"]['topLevelComment']["snippet"]['likeCount']
         replies = i["snippet"]['totalReplyCount']
        
 
-        box.append([name, comment, published_at, likes, replies])
+        box.append([name, comment, comment_id, published_at, likes, replies])
        
         totalReplyCount = i["snippet"]['totalReplyCount']
 
@@ -71,11 +72,12 @@ def scrape_comments_with_replies(url):
             for i in data2["items"]:
                 name = i["snippet"]["authorDisplayName"]
                 comment = i["snippet"]["textDisplay"]
+                comment_id = i["id"]
                 published_at = i["snippet"]['publishedAt']
                 likes = i["snippet"]['likeCount']
                 replies = ""
 
-                box.append([name, comment, published_at, likes, replies])
+                box.append([name, comment, comment_id, published_at, likes, replies])
 
     while ("nextPageToken" in data):
 
@@ -85,6 +87,7 @@ def scrape_comments_with_replies(url):
         for i in data["items"]:
             name = i["snippet"]['topLevelComment']["snippet"]["authorDisplayName"]
             comment = i["snippet"]['topLevelComment']["snippet"]["textDisplay"]
+            comment_id = i["id"]
             published_at = i["snippet"]['topLevelComment']["snippet"]['publishedAt']
             likes = i["snippet"]['topLevelComment']["snippet"]['likeCount']
             replies = i["snippet"]['totalReplyCount']
@@ -103,6 +106,7 @@ def scrape_comments_with_replies(url):
                 for i in data2["items"]:
                     name = i["snippet"]["authorDisplayName"]
                     comment = i["snippet"]["textDisplay"]
+                    comment_id = i["id"]
                     published_at = i["snippet"]['publishedAt']
                     likes = i["snippet"]['likeCount']
                     replies = ''
@@ -111,8 +115,8 @@ def scrape_comments_with_replies(url):
 
 
 
-    df = pd.DataFrame({'Name': [i[0] for i in box], 'Comment': [i[1] for i in box], 'Time': [i[2] for i in box],
-                       'Likes': [i[3] for i in box], 'Reply Count': [i[4] for i in box]})
+    df = pd.DataFrame({'Name': [i[0] for i in box], 'Comment': [i[1] for i in box],'Comment Id':[i[2] for i in box], 'Time': [i[3] for i in box],
+                       'Likes': [i[4] for i in box], 'Reply Count': [i[5] for i in box]})
     
     df.index = np.arange(1, len(df)+1)   
     
