9561ccbe8fe671f23ebaa2d3fffd573d3e5d5ba2
shres707 <mailshreekanth@gmail.com>
2024-08-03 20:34:11 +0530

    Update automatic_reply_app.py

--- automatic_reply_app.py
+++ automatic_reply_app.py
@@ -11,20 +11,15 @@ from transformers import pipeline,AutoTokenizer,AutoModelForCausalLM
 cohere_api_key = "eH1W45sG4i7AiEgI776DKgExK22QsTkCfWWdp7ue"
 co = cohere.Client(cohere_api_key)
 
-
-def app():
-  st.title("Automatic Reply")
-  df_clean=st.session_state.test
-  latest_comment = df_clean['Comment'].values[0]
-  st.write('Latest Comment: '+latest_comment)
-  prompt_template= f"""
-    You are a helpful YouTube comment replying bot. Please provide a response based on the following conditions:
-    - If the comment is appreciative, positive feedback, or neutral, reply 'Thanks for your feedback.'
-    - If the comment is critical feedback, reply 'Sorry for the inconvenience. We will look into the issue and get back to you.'
-    - If the comment is a question, try to answer the question in 25 words. If you don't know the answer, say you don't know.
-    Comment: {latest_comment}
-    Reply:
-    """
+def generate_reply(comment):
+  prompt_template = f"""
+      You are a helpful YouTube comment replying bot. Please provide a response based on the following conditions:
+      - If the comment is appreciative, positive feedback, or neutral, reply 'Thanks for your feedback.'
+      - If the comment is critical feedback, reply 'Sorry for the inconvenience. We will look into the issue and get back to you.'
+      - If the comment is a question, try to answer the question in 25 words. If you don't know the answer, say you don't know.
+      Comment: {latest_comment}
+      Reply:
+      """
 
   response = co.generate(
     model='command-xlarge-nightly',
@@ -36,7 +31,20 @@ def app():
     stop_sequences=["Reply:"]
   )
   reply = response.generations[0].text.strip()
+  return reply
 
 
 
-  st.write("reply:"+reply)
+def app():
+  st.title("Automatic Reply")
+  df_clean=st.session_state.test
+  latest_comment = df_clean['Comment'].values[0]
+  st.write('Latest Comment: '+latest_comment)
+
+  st.text_area('Latest Comment', latest_comment, key='latest_comment_display', height=100)
+  if st.button('Generate Reply'):
+    comment = st.session_state.latest_comment_display
+    reply = generate_reply(comment)
+    st.text_area("Reply", reply if reply else "No reply generated.", height=100)
+
+
