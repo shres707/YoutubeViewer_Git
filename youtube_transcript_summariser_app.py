43bcd565742ef9fb4d56306cba3de9566cf65b1b
shres707 <mailshreekanth@gmail.com>
2024-08-12 11:36:04 +0530

    Update youtube_transcript_summariser_app.py

--- youtube_transcript_summariser_app.py
+++ youtube_transcript_summariser_app.py
@@ -5,7 +5,7 @@ co = cohere.Client(cohere_api_key)
 
 def app():
   st.title("YouTube Transcript Summariser")
-  transscript_data = st.session_state.transcript_data
+  transript_data = st.session_state.transcript_data
 
   response = co.summarize(
       text=transcript_data,
