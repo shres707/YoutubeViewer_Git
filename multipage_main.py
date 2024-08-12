a189616f4ea637cf871b839abbe723dcfe933143
shres707 <mailshreekanth@gmail.com>
2024-08-13 00:04:01 +0530

    updated with chatbot

--- multipage_main.py
+++ multipage_main.py
@@ -4,6 +4,7 @@ import data_preprocess_eda_app
 import sentiment_app
 import automatic_reply_app
 import youtube_transcript_summariser_app
+import youtube_comment_chatbot_app
 
 
 import streamlit as st
@@ -13,7 +14,8 @@ PAGES = {
     "EDA"  : eda_app,
     "Sentiment Analysis": sentiment_app,
     "Automatic Reply": automatic_reply_app,
-    "Transcript Summariser": youtube_transcript_summariser_app
+    "Transcript Summariser": youtube_transcript_summariser_app,
+    "Comment ChatBot": youtube_comment_chatbot_app
 
 }
 st.sidebar.title('CRISP DM Navigation')
