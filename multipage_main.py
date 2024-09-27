c673e152278e5865d66fc65cb5f2321d254915a6
shres707 <95143327+shres707@users.noreply.github.com>
2024-09-27 12:33:44 +0530

    Comment

--- multipage_main.py
+++ multipage_main.py
@@ -4,7 +4,7 @@ import data_preprocess_eda_app
 import sentiment_app
 #import automatic_reply_app
 #import youtube_transcript_summariser_app
-import youtube_comment_chatbot_app
+#import youtube_comment_chatbot_app
 
 
 import streamlit as st
@@ -15,7 +15,7 @@ PAGES = {
     "Sentiment Analysis": sentiment_app,
     #"Automatic Reply": automatic_reply_app,
     #"Transcript Summariser": youtube_transcript_summariser_app,
-    "Comment ChatBot": youtube_comment_chatbot_app
+    #"Comment ChatBot": youtube_comment_chatbot_app
 
 }
 st.sidebar.title('CRISP DM Navigation')
